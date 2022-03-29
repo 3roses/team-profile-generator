08b3026a6667bc98c7056bbf8e0dfc2d4db33dc8
Craig Leslie <cleslie25@gmail.com>
2022-03-29 00:19:20 -0500

    Refactored .then blocks

--- index.js
+++ index.js
@@ -4,34 +4,133 @@ const fs = require('fs');
 const jest = require('jest');
 
 
-
-
-const userQuestions = () => {
-    response = inquirer.prompt([
+const managerQuestions = () => {
+    manager = inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: "What is this team member's email?"
+            message: "What is the team manager's name?"
         },
         {
             type: 'input',
             name: 'id',
-            message: "What is this team member's id?"
+            message: "What is the team manager's id?"
         },
         {
             type: 'input',
             name: 'email',
-            message: "What is this team member's email?"
+            message: "What is the team manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the team manager's office number?"
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'Continue building with an engineer, intern, or finish your team?',
+            choices: ['Engineer', 'Intern', 'Team is complete']
+        }
+    ]).then( response => {
+        if (response.employeeType === 'Engineer'){
+            engineerQuestions()
+        }else if (response.employeeType === 'Intern'){
+            internQuestions()
+        }else{
+            console.log('Goodluck to your team!')
+        }
+    })
+}
+
+const engineerQuestions = () =>{
+    engineer = inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's id?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email?"
         },
         {
             type: 'input',
             name: 'gitHub',
-            message: "What is this team member's gitHub?"
+            message: "What is the engineer's gitHub?"
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'Continue building with an engineer, intern, or finish your team?',
+            choices: ['Engineer', 'Intern', 'Team is complete']
         }
-    ])
+    ]).then( response => {
+        if (response.employeeType === 'Engineer'){
+            engineerQuestions()
+        }else if (response.employeeType === 'Intern'){
+            internQuestions()
+        }else{
+            console.log('Goodluck to your team!')
+        }
+    })
+}
+
+const internQuestions = () => {
+    intern = inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the Intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the Intern's id?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Intern's email?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Where does the intern attend school?"
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'Continue building with an engineer, intern, or finish your team?',
+            choices: ['Engineer', 'Intern', 'Team is complete']
+        }
+    ]).then( response => {
+        if (response.employeeType === 'Engineer'){
+            engineerQuestions()
+        }else if (response.employeeType === 'Intern'){
+            internQuestions()
+            console.log(response)
+        }else{
+            console.log('Goodluck to your team!')
+        }
+    })
 }
 
 
-let htmFile = (teamMembers) => (
+const init = () =>{
+    managerQuestions()
+}
+
+init()
+
+
+let htmFile = ({name, id, email, officeNumber}) => (
     ``
 )
+
