ff1416566b4f04e7daec1fdf81ba573e8cf3e4c1
Craig Leslie <cleslie25@gmail.com>
2022-03-30 13:35:09 -0500

    Render html Created methods inside each class that creates html for their specific card. Wrote a function that writes this html to a file upon completion of prompts. Each time a prompt is completed, a new object is instantiated.

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const allData = { manager: {}, engineers: [], interns: [] };
 
 const managerQuestions = () => {
-    manager = inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -33,18 +38,21 @@ const managerQuestions = () => {
             choices: ['Engineer', 'Intern', 'Team is complete']
         }
     ]).then( response => {
+        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        allData.manager = manager;
+
         if (response.employeeType === 'Engineer'){
             engineerQuestions()
         }else if (response.employeeType === 'Intern'){
             internQuestions()
         }else{
-            console.log('Goodluck to your team!')
+            renderHtml (allData)
         }
     })
 }
 
 const engineerQuestions = () =>{
-    engineer = inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -72,18 +80,21 @@ const engineerQuestions = () =>{
             choices: ['Engineer', 'Intern', 'Team is complete']
         }
     ]).then( response => {
+        const eng = new Engineer(response.name, response.id, response.email, response.gitHub)
+        allData.engineers.push(eng);
+
         if (response.employeeType === 'Engineer'){
             engineerQuestions()
         }else if (response.employeeType === 'Intern'){
             internQuestions()
         }else{
-            console.log('Goodluck to your team!')
+            renderHtml (allData)
         }
     })
 }
 
 const internQuestions = () => {
-    intern = inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -111,26 +122,48 @@ const internQuestions = () => {
             choices: ['Engineer', 'Intern', 'Team is complete']
         }
     ]).then( response => {
+        const intern = new Intern(response.name, response.id, response.email, response.school)
+        allData.interns.push(intern);
+
         if (response.employeeType === 'Engineer'){
             engineerQuestions()
         }else if (response.employeeType === 'Intern'){
             internQuestions()
-            console.log(response)
         }else{
-            console.log('Goodluck to your team!')
+            renderHtml (allData)
         }
     })
 }
 
 
-const init = () =>{
+
+
+function writeHTMLToFile (htmlStr) {
+    fs.writeFile('dist/index.html', htmlStr, (err) => {
+		err ? console.log(err) : console.log('Your team cards have been created!');
+	})
+}
+
+
+function renderHtml (allData) {
+    const manager = allData.manager;
+    const allEngineers = allData.engineers;
+    const allInterns = allData.interns;
+    
+    var htmlStr = `
+        ${manager.getHtml()}
+        ${allEngineers.map(engineer => engineer.getHtml())}
+        ${allInterns.map(intern => intern.getHtml())}
+        </div>
+
+        </body>
+        </html>
+        `
+    writeHTMLToFile(htmlStr)
+}
+
+const init = () => {
     managerQuestions()
 }
 
 init()
-
-
-let htmFile = ({name, id, email, officeNumber}) => (
-    ``
-)
-
